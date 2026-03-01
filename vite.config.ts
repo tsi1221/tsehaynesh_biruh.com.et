26e2188424f65e645c4892ec0dc28dddf3d3e546
tsi1221 <tsehayneshbiruh2@gmail.com>
2026-03-01 08:06:44 -0500

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'; // Import the plugin
 
-// https://vitejs.dev
 export default defineConfig({
-  plugins: [tailwindcss(), react()], // Add the plugin here
+  plugins: [react()],
 });
